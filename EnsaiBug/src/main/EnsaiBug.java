ca826a9fb06bff1cadd53913b41c4c955e15a906
Administrateur <Administrateur@192.168.60.254>
2013-12-19 12:15:54 +0100

    Voila

---
+++ EnsaiBug/src/main/EnsaiBug.java
@@ -0,0 +1,26 @@
+package main;
+
+public class EnsaiBug {
+	
+	public  void chiasse(){
+		int res=0;
+		return(res);
+	}
+	
+	public int bite(){
+	
+	}
+	
+	public int pd(){
+		String ch="bande de pd";
+		return(ch);
+	}
+
+	public static void main(String[] args) {
+		chiasse();
+		bite();
+		pd();
+		tocard(bite());
+	}
+
+}
